7b9189e11ad4967c4a7030f54d3d2606864aa3ea
HuuDung <huudung2411@gmail.com>
2018-03-09 15:02:46 +0700

    update

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -25,23 +25,11 @@
                         <div class="box-body box-profile">
                             <img class="profile-user-img img-responsive img-circle" src="{{asset('storage/'.$user->avatar)}}" alt="User profile picture">
 
-                            <h3 class="profile-username text-center">{{$user->name}}</h3>
+                            <h3 class="profile-username text-center">{{ $user->name }}</h3>
 
                             <p class="text-muted text-center">Software Engineer</p>
 
-                            <ul class="list-group list-group-unbordered">
-                                <li class="list-group-item">
-                                    <b>Followers</b> <a class="pull-right">1,322</a>
-                                </li>
-                                <li class="list-group-item">
-                                    <b>Following</b> <a class="pull-right">543</a>
-                                </li>
-                                <li class="list-group-item">
-                                    <b>Friends</b> <a class="pull-right">13,287</a>
-                                </li>
-                            </ul>
-
-                            <a href="#" class="btn btn-primary btn-block"><b>Follow</b></a>
+                            {{--<a href="#" class="btn btn-primary btn-block"><b>Follow</b></a>--}}
                         </div>
                         <!-- /.box-body -->
                     </div>
@@ -311,10 +299,10 @@
                             <div class="tab-pane" id="settings">
                                 <form class="form-horizontal">
                                     <div class="form-group">
-                                        <label for="inputName" class="col-sm-2 control-label">Name</label>
+                                        <label for="name" class="col-sm-2 control-label">Name</label>
 
                                         <div class="col-sm-10">
-                                            <input type="email" class="form-control" id="inputName" placeholder="Name">
+                                            <input type="text" class="form-control" id="name" placeholder="{{ $user->name }}">
                                         </div>
                                     </div>
                                     <div class="form-group">
